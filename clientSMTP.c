abf48837c1190ef756221c548d5b305888c7f571
Sarikaya_Fatih <fatih_1894@hotmail.com>
2018-11-16 18:13:53 +0100

    fin test sur dns

--- clientSMTP.c
+++ clientSMTP.c
@@ -3,10 +3,13 @@
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <netdb.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 #define NOMBRE_ARGUMENT 2
 
-void erreur(char * message_erreur, int nombre_erreur);
+void erreur(char * message_erreur);
 
 int main(int argc , char** argv)
 {
@@ -18,41 +21,44 @@ int main(int argc , char** argv)
   char* address_internet = argv[1];// les arguments
   char* fichier_email = argv[2];
 
-  struct hostent* adresse_ip_info = gethostbyname(adress_internet);
+  struct hostent* adresse_ip_info = gethostbyname(address_internet);
 
   if(adresse_ip_info == NULL)
   {
-    int err = h_errno;
-    erreur("Erreur lors de la demande de l'adresse ip",err);
+    erreur("Erreur lors de la demande de l'adresse ip");
   }
 
   if(adresse_ip_info->h_addrtype & AF_INET)
   {
-    printf("type adresse IPv4 /n");
+    printf("type adresse IPv4 \n");
   }
   else if(adresse_ip_info->h_addrtype & AF_INET6 )
   {
-    printf("type adresse IPv6 /n");
+    printf("type adresse IPv6 \n");
   }
   else
   {
-    printf("type adresse inconnu /n");
+    printf("type adresse inconnu \n");
   }
+printf(" test length %d \n",adresse_ip_info->h_length);
 
-  for(int i=0; i < adresse_ip_info->h_length,++i)
-  {
+  int i=0;
     printf(adresse_ip_info->h_aliases[i]);
     printf(" : ");
-    printf(adresse_ip_info->h_addrtype[i] );
-    printf("/n");
-  }
+    if(adresse_ip_info->h_addr_list[i] != NULL)
+    {
+    printf(adresse_ip_info->h_addr_list[i] );
 
+    printf("\n");
+  }
+printf("test de fin \n");
 
 }
 
-void erreur(char* message_erreur, int nombre_erreur)
+void erreur(char* message_erreur)
 {
+  int nombre_erreur = h_errno;
   fprintf(stderr,message_erreur);
-  fprintf(stderr," %s /n",strerror(nombre_erreur));
+  fprintf(stderr," %s \n",hstrerror(nombre_erreur));
   exit(EXIT_FAILURE);
 }
